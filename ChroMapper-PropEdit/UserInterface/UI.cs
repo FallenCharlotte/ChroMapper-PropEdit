7970297ed092191bf6597964d6d720b30dbe9537
Moizac <moistsac@gmail.com>
2022-10-23 19:08:41 -0700

    Wow scroll

--- ChroMapper-PropEdit/UserInterface/UI.cs
+++ ChroMapper-PropEdit/UserInterface/UI.cs
@@ -13,18 +13,13 @@ public class UI {
 	public ExtensionButton main_button;
 	public GameObject window;
 	public GameObject none_status;
+	public GameObject title;
 	
 	public UI() {
-		Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("ChroMapper_PropEdit.Resources.Icon.png");
-		byte[] data = new byte[stream.Length];
-		stream.Read(data, 0, (int)stream.Length);
-		
-		Texture2D texture2D = new Texture2D(256, 256);
-		texture2D.LoadImage(data);
-		
-		var _sprite = Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), new Vector2(0, 0), 100.0f);
-		
-		main_button = ExtensionButtons.AddButton(_sprite, "Prop Edit", ToggleWindow);
+		main_button = ExtensionButtons.AddButton(
+			LoadSprite("ChroMapper_PropEdit.Resources.Icon.png"),
+			"Prop Edit",
+			ToggleWindow);
 	}
 	
 	public void AddWindow(MapEditorUI mapEditorUI) {
@@ -37,16 +32,75 @@ public class UI {
 		window.GetComponent<DragWindowController>().canvas = parent.GetComponent<Canvas>();
 		//window.GetComponent<DragWindowController>().OnDragWindow += AnchoredPosSave;
 
-		AttachTransform(window, 220, 256, 1, 0, -5, 5, 1, 0);
-		var image = window.AddComponent<Image>();
-
-		image.sprite = PersistentUI.Instance.Sprites.Background;
-		image.type = Image.Type.Sliced;
-		image.color = new Color(0.24f, 0.24f, 0.24f, 1);
+		AttachTransform(window, new Vector2(220, 256), new Vector2(0, 0), new Vector2(0.5f, 0.5f));
+		{
+			var image = window.AddComponent<Image>();
+			image.sprite = PersistentUI.Instance.Sprites.Background;
+			image.type = Image.Type.Sliced;
+			image.color = new Color(0.24f, 0.24f, 0.24f, 1);
+		}
 		
-		window.SetActive(false);
+		//window.SetActive(false);
 		
-		none_status = AddLabel(window.transform, "NoneStatus", "No objects selected", new Vector2(0, -20));
+		title = AddLabel(window.transform, "Title", "Prop Editor", new Vector2(10, -20), size: new Vector2(-10, 30), font_size: 28, anchor_min: new Vector2(0, 1), anchor_max: new Vector2(1, 1), align: TextAlignmentOptions.Left);
+		
+		var container = AddChild(window, "Prop Scroll Container");
+		AttachTransform(container, new Vector2(-10, -40), new Vector2(0, -15), new Vector2(0, 0), new Vector2(1, 1));
+		{
+			var image = container.AddComponent<Image>();
+			image.sprite = PersistentUI.Instance.Sprites.Background;
+			image.type = Image.Type.Sliced;
+			image.color = new Color(0.1f, 0.1f, 0.1f, 1);
+		}
+		
+		var scroll_area = AddChild(container, "Scroll Area", typeof(ScrollRect));
+		AttachTransform(scroll_area, new Vector2(0, -10), new Vector2(0, 0), new Vector2(0, 0), new Vector2(1, 1));
+		var mask = scroll_area.AddComponent<RectMask2D>();
+		var srect = scroll_area.GetComponent<ScrollRect>();
+		srect.vertical = true;
+		srect.horizontal = false;
+		srect.verticalScrollbarVisibility = ScrollRect.ScrollbarVisibility.AutoHide;
+		
+		var panel = AddChild(scroll_area, "Prop Panel");
+		srect.content = AttachTransform(panel, new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 1), new Vector2(1, 1));
+		{
+			var layout = panel.AddComponent<VerticalLayoutGroup>();
+			layout.padding = new RectOffset(5, 5, 0, 0);
+			layout.spacing = 0;
+			layout.childControlHeight = false;
+			layout.childForceExpandWidth = true;
+		}
+		{
+			var fitter = panel.AddComponent<ContentSizeFitter>();
+			fitter.verticalFit = ContentSizeFitter.FitMode.PreferredSize;
+		}
+		
+		var scroller = AddChild(container, "Scroll Bar", typeof(Scrollbar));
+		AttachTransform(scroller, new Vector2(10, 0), new Vector2(-5.5f, 0), new Vector2(1, 0), new Vector2(1, 1));
+		var scrollbar = scroller.GetComponent<Scrollbar>();
+		scrollbar.transition = Selectable.Transition.ColorTint;
+		scrollbar.direction = Scrollbar.Direction.BottomToTop;
+		srect.verticalScrollbar = scrollbar;
+		
+		var slide = AddChild(scroller, "Slide");
+		AttachTransform(slide, new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 0), new Vector2(1, 1));
+		{
+			var image = slide.AddComponent<Image>();
+			image.sprite = PersistentUI.Instance.Sprites.Background;
+			image.type = Image.Type.Sliced;
+			image.color = new Color(0.24f, 0.24f, 0.24f, 1);
+		}
+		
+		var handle = AddChild(slide, "Handle", typeof(Canvas));
+		AttachTransform(handle, new Vector2(0, 0), new Vector2(0, 0), new Vector2(0, 0), new Vector2(1, 1));
+		{
+			var image = handle.AddComponent<Image>();
+			image.sprite = PersistentUI.Instance.Sprites.Background;
+			image.type = Image.Type.Sliced;
+			image.color = new Color(0.7f, 0.7f, 0.7f, 1);
+			scrollbar.targetGraphic = image;
+			scrollbar.handleRect = handle.GetComponent<RectTransform>();
+		}
 		
 		UpdateSelection();
 	}
@@ -57,52 +111,66 @@ public class UI {
 	
 	public void UpdateSelection() {
 		if (SelectionController.HasSelectedObjects()) {
-			none_status.SetActive(false);
+			//none_status.SetActive(false);
 		}
 		else {
-			none_status.SetActive(true);
+			//none_status.SetActive(true);
 		}
 	}
 	
-	private GameObject AddLabel(Transform parent, string title, string text, Vector2 pos, Vector2? size = null)
-	{
+	private GameObject AddChild(GameObject parent, string name, params System.Type[] components) {
+		var obj = new GameObject(name, components);
+		obj.transform.SetParent(parent.transform);
+		return obj;
+	}
+	
+	private GameObject AddLabel(Transform parent, string title, string text, Vector2 pos, Vector2? anchor_min = null, Vector2? anchor_max = null, int font_size = 14, Vector2? size = null, TextAlignmentOptions align = TextAlignmentOptions.Center) {
 		var entryLabel = new GameObject(title + " Label", typeof(TextMeshProUGUI));
 		var rectTransform = ((RectTransform)entryLabel.transform);
 		rectTransform.SetParent(parent);
-
-		MoveTransform(rectTransform, 110, 24, 0.5f, 1, pos.x, pos.y);
+		
+		MoveTransform(rectTransform, size ?? new Vector2(110, 24), pos, anchor_min ?? new Vector2(0.5f, 1), anchor_max ?? new Vector2(0.5f, 1));
 		var textComponent = entryLabel.GetComponent<TextMeshProUGUI>();
-
+		
 		textComponent.name = title;
 		textComponent.font = PersistentUI.Instance.ButtonPrefab.Text.font;
-		textComponent.alignment = TextAlignmentOptions.Center;
-		textComponent.fontSize = 14;
+		textComponent.alignment = align;
+		textComponent.fontSize = font_size;
 		textComponent.text = text;
 		
 		return entryLabel;
 	}
 	
-	private RectTransform AttachTransform(GameObject obj, float width, float height, float anchorX, float anchorY, float x, float y, float p1 = 0.5f, float p2 = 0.5f)
+	private RectTransform AttachTransform(GameObject obj,    Vector2 size, Vector2 pos, Vector2? anchor_min = null, Vector2? anchor_max = null, Vector2? pivot = null)
 	{
-		var rectTransform = obj.AddComponent<RectTransform>();
-		rectTransform.localScale = new Vector3(1, 1, 1);
-		rectTransform.sizeDelta = new Vector2(width, height);
-		rectTransform.pivot = new Vector2(p1, p2);
-		rectTransform.anchorMin = rectTransform.anchorMax = new Vector2(anchorX, anchorY);
-		rectTransform.anchoredPosition = new Vector3(x, y, 0);
-
-		return rectTransform;
+		var rectTransform = obj.GetComponent<RectTransform>();
+		if (rectTransform == null) {
+			rectTransform = obj.AddComponent<RectTransform>();
+		}
+		return MoveTransform(rectTransform, size, pos, anchor_min, anchor_max, pivot);
 	}
 	
-	private RectTransform MoveTransform(RectTransform rectTransform, float width, float height, float anchorX, float anchorY, float x, float y, float p1 = 0.5f, float p2 = 0.5f)
+	private RectTransform MoveTransform(RectTransform trans, Vector2 size, Vector2 pos, Vector2? anchor_min = null, Vector2? anchor_max = null, Vector2? pivot = null)
 	{
-		rectTransform.localScale = new Vector3(1, 1, 1);
-		rectTransform.sizeDelta = new Vector2(width, height);
-		rectTransform.pivot = new Vector2(p1, p2);
-		rectTransform.anchorMin = rectTransform.anchorMax = new Vector2(anchorX, anchorY);
-		rectTransform.anchoredPosition = new Vector3(x, y, 0);
-
-		return rectTransform;
+		trans.localScale = new Vector3(1, 1, 1);
+		trans.sizeDelta = size;
+		trans.pivot = pivot ?? new Vector2(0.5f, 0.5f);
+		trans.anchorMin = anchor_min ?? new Vector2(0, 0);
+		trans.anchorMax = anchor_max ?? anchor_min ?? new Vector2(1, 1);
+		trans.anchoredPosition = new Vector3(pos.x, pos.y, 0);
+		
+		return trans;
+	}
+	
+	private Sprite LoadSprite(string asset) {
+		Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(asset);
+		byte[] data = new byte[stream.Length];
+		stream.Read(data, 0, (int)stream.Length);
+		
+		Texture2D texture2D = new Texture2D(256, 256);
+		texture2D.LoadImage(data);
+		
+		return Sprite.Create(texture2D, new Rect(0, 0, texture2D.width, texture2D.height), new Vector2(0, 0), 100.0f);
 	}
 }
 

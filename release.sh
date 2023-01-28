aa5c159fe1e4e6283e6752cb826947d3f59b5cfc
Moizac <moistsac@gmail.com>
2023-01-28 15:05:14 -0800

    Release version script

---
+++ release.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROJECT="ChroMapper-PropEdit"
+ver=$1
+short=$1
+
+while [ "$(echo "$ver" | tr -dc '.' | awk '{ print length; }')" -lt "3" ]
+do
+	ver="${ver}.0"
+done
+
+sed -i 's/AssemblyVersion(.*)/AssemblyVersion("'$ver'")/' $PROJECT/Properties/AssemblyInfo.cs
+sed -i 's/AssemblyFileVersion(.*)/AssemblyFileVersion("'$ver'")/' $PROJECT/Properties/AssemblyInfo.cs
+
+git add $PROJECT/Properties/AssemblyInfo.cs
+git commit -m "v${short}"
+git tag "v${short}"
+msbuild

ba97afd0d7ee336f3aefdd3d70c3a9e3d4de0d84
Moizac <moistsac@gmail.com>
2023-04-20 11:08:54 -0700

    vtest

--- ChroMapper-PropEdit/Properties/AssemblyInfo.cs
+++ ChroMapper-PropEdit/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.3.12.2")]
-[assembly: AssemblyVersion("0.3.12.2")]
-[assembly: AssemblyFileVersion("0.3.12.2")]
+// [assembly: AssemblyVersion("test")]
+[assembly: AssemblyVersion("test")]
+[assembly: AssemblyFileVersion("test")]

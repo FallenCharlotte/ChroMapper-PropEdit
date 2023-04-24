559d90080ab1311b7af364354073e07c8fdc8984
Moizac <moistsac@gmail.com>
2023-04-24 15:28:16 -0700

    Git hooks don't work so good

--- release.sh
+++ release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 PROJECT="$(basename "$(pwd)")"
+CHANGELOG="Changelog.txt"
 ver=$1
 short=$1
 
@@ -11,11 +12,16 @@ done
 
 sed -i 's/AssemblyVersion(.*)/AssemblyVersion("'$ver'")/' $PROJECT/Properties/AssemblyInfo.cs
 sed -i 's/AssemblyFileVersion(.*)/AssemblyFileVersion("'$ver'")/' $PROJECT/Properties/AssemblyInfo.cs
-sed -i -e "1i v${short}" Changelog
-cat Changelog > Changelog.txt
-sed -i '1i\\' Changelog
 
-git add $PROJECT/Properties/AssemblyInfo.cs Changelog
+mv "$CHANGELOG"{,.old}
+echo "v${short}" > "$CHANGELOG"
+git log $(git describe --tags --abbrev=0)..HEAD --pretty=format:'	%s' >> "$CHANGELOG"
+echo -e "\n" >> "$CHANGELOG"
+cat Changelog.txt.old >> "$CHANGELOG"
+rm "$CHANGELOG".old
+$EDITOR "$CHANGELOG"
+
+git add $PROJECT/Properties/AssemblyInfo.cs "$CHANGELOG"
 git commit -nm "v${short}"
 git tag "v${short}"
 msbuild

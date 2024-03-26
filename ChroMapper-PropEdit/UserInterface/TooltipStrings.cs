39b8ce8f607d8c5d09371a495fc2f7327576da41
MitchSirco <116924507+MitchSirco@users.noreply.github.com>
2024-03-26 14:23:08 +0100

    Added Custom Events

--- ChroMapper-PropEdit/UserInterface/TooltipStrings.cs
+++ ChroMapper-PropEdit/UserInterface/TooltipStrings.cs
@@ -118,7 +118,46 @@ namespace ChroMapper_PropEdit.UserInterface {
 
 			BPMChange,
 
+			TrackDuration,
+			TrackEasing,
+			TrackProperty,
+			TrackRepeat,
+
+			AnimateTrackPosition,
+			AnimateTrackRotation,
+			AnimateTrackLocalRotation,
+			AnimateTrackScale,
+			AnimateTrackDissolve,
+			AnimateTrackDissolveArrow,
+			AnimateTrackInteractable,
+			AnimateTrackTime,
+
+			AssignTrackParentParent,
+			AssignTrackChildren,
+			AssignTrackKeepPosition,
+
+
+			V2AssignFogTrackAttenuation,
+			V2AssignFogTrackOffset,
+			V2AssignFogTrackStartY,
+			V2AssignFogTrackHeight,
 			
+			AssignPathAnimationDefinitePosition,
+
+
+			AssignPlayerToTrackTrack,
+			AssignPlayerToTrackTarget,
+
+			//There is another track called AnimateComponent, https://github.com/Aeroluna/Heck/wiki/Environment/#Components
+			//https://github.com/Aeroluna/Heck/wiki/Animation#AnimateComponent
+			AnimateComponentILightWithId,
+			AnimateComponentLightID,
+			AnimateComponentType,
+			AnimateComponentBloomFogEnvironment,
+			AnimateComponentTubeBloomPrePassLight,
+			AnimateComponentTubeBloomPrePassLightColorAlphaMultiplier,
+			AnimateComponentTubeBloomPrePassLightBloomFogIntensityMultiplier,
+
 
 		}
 
@@ -211,12 +250,73 @@ namespace ChroMapper_PropEdit.UserInterface {
 			//BPM Changes
 			{Tooltip.BPMChange, "Alters the BPM to the defined value at the indicated beat." },
 
-			};
-		public string GetTooltip(string BSobject, Tooltip property) {
-			Console.WriteLine(string.Format(name[property], objects[BSobject]));
-			//Assuming you want to replace {0} in the name dictionary with BSobject
+			//Custom Events
+			{Tooltip.TrackDuration, "The length of the {0} in beats (defaults to 0)" },
+			{Tooltip.TrackEasing, "An easing for the animation to follow (defaults to easeLinear)." },
+			{Tooltip.TrackProperty, "The property you want to animate." },
+			{Tooltip.TrackRepeat, "How many times to repeat this {0} (defaults to 0)" },
 
-			return string.Format(name[property], objects[BSobject]);
+			//AnimateTrack
+			{Tooltip.AnimateTrackPosition, "Describes the position offset of an object. It will continue any normal movement and have this stacked on top of it." },
+			{Tooltip.AnimateTrackLocalRotation, "describes the local rotation offset of an object. This means it is rotated with itself as the origin. Uses euler values. Do note that the note spawn effect will be rotated accordlingly. Notes attempting to look towards the player may look strange, you can disable their look with disableNoteLook." },
+			{Tooltip.AnimateTrackRotation, "Describes the world rotation offset of an object. This means it is rotated with the world as the origin. Uses euler values. Think of 360 mode." },
+			{Tooltip.AnimateTrackScale, "Decribes the scale of an object. This will be based off their initial size. A scale of 1 is equal to normal size, anything under is smaller, over is larger." },
+			{Tooltip.AnimateTrackDissolve, "Controls the dissolve effect on both notes and walls. It's the effect that happens when things go away upon failing a song. Keep in mind that notes and the arrows on notes have seperate dissolve properties, see dissolveArrow. \n Note: How this looks will depend on the player's graphics settings." },
+			{Tooltip.AnimateTrackDissolveArrow, "Controls the dissolve effect on the arrows of notes. Similar to the look of the disappearing notes modifier. Has no effect on obstacles" },
+			{Tooltip.AnimateTrackInteractable, "Controls whether or not the player can interact with the note/obstacle." },
+			{Tooltip.AnimateTrackTime, "lets you control what point in the note's \"lifespan\" it is at a given time. Lifespan examples: 0->note spawns at its earliest point \n 0.25->note is halfway by the player \n 0.5->note is on the platform the player stands \n 0.75-> note about to go \n 1-> Note gone.  It is worth noting that every object on one track will get the same time values when animating this property. This means they would suddenly appear to all be at the same point. It is recommended for every object to have its own track when using _time." },
+			
+			//AssignTrackParent
+			{Tooltip.AssignTrackParentParent, "The track you want to animate." },
+			{Tooltip.AssignTrackChildren, "An array of tracks that get inherited by the ParentTrack." },
+			{Tooltip.AssignTrackKeepPosition, "If activated, the parent-relative position, scale and rotation are modified such that the object keeps the same world space position, rotation and scale as before." },
+
+			//AssingFogTrack (outdated but used in AnimateComponent)
+			{Tooltip.V2AssignFogTrackAttenuation, "controls the fog density. Is calculated logarithmic" },
+			{Tooltip.V2AssignFogTrackOffset, "controls the \"offset\" property of fog." },
+			{Tooltip.V2AssignFogTrackStartY, "startY is starting Y of the gradient thing." },
+			{Tooltip.V2AssignFogTrackHeight, "controls the height is the gradient length of the dissolving plane fog." },
+
+			//AssignPlayerToTrack
+			{Tooltip.AssignPlayerToTrackTrack, "The track you wish to assign the player to." },
+			{Tooltip.AssignPlayerToTrackTarget, "(optional) The specific player object you wish to target. Available targets are \"Root\", \"Head\", \"LeftHand\", and \"RightHand\"." },
+
+
+			{Tooltip.AssignPathAnimationDefinitePosition, "Describes the definite position of an object. Will completely overwrite the object's default movement. However, this does take into account lineIndex/lineLayer and world rotation." },
+
+
+			//AnimateComponent
+			{Tooltip.AnimateComponentILightWithId, "Activates the lightIdInput" }, //Unsure
+			{Tooltip.AnimateComponentLightID, "Which ID to assign. For use with the lightID tag for lighting events (Not animateable)." },
+			{Tooltip.AnimateComponentType, "Which event type to active on. (Not animateable)" }, //unsure, note type or if its obstacle or note?
+			{Tooltip.AnimateComponentBloomFogEnvironment, "Activates the BloomFogEnvironment Input. Will always be found on the [0]Environment object" },
+			{Tooltip.AnimateComponentTubeBloomPrePassLight, "Activates the TubeBloomPrePassLight input." },
+			{Tooltip.AnimateComponentTubeBloomPrePassLightColorAlphaMultiplier, "Changes the multiplier for the color ." }, //Unsure
+			{Tooltip.AnimateComponentTubeBloomPrePassLightBloomFogIntensityMultiplier, "Changes the multiplier for the fog intensity" },
+
+
+
+			};
+		public string GetTooltip(string BSobject, Tooltip property, string foreachString = "", string foreachKey = "") {
+
+			//Normal case
+			if (foreachString == "") {
+				Console.WriteLine(string.Format(name[property], objects[BSobject]));
+				return string.Format(name[property], objects[BSobject]);
+			}
+
+			// for foreach cases
+			Tooltip tooltipEnum;
+			string enumString = foreachKey + foreachString.Replace(" ", "");
+			if (Enum.TryParse(enumString, true, out tooltipEnum)) {
+				Console.WriteLine(string.Format(name[tooltipEnum], objects[BSobject]));
+				return string.Format(name[tooltipEnum], objects[BSobject]);
+			}
+			else {
+				// Handle the case where the string does not match any enum value
+				Console.WriteLine($"The string '{enumString}' does not match any Tooltip enum value.");
+				return string.Empty; // or return a default value
+			}
 		}
 	}
 }

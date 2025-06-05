02dc2df7ebab291d2826addf3e74a1a3017a7363
Charlotte <fallencharlotte@pm.me>
2025-06-05 12:01:30 -0700

    Zip packaging

--- release.sh
+++ release.sh
@@ -39,6 +39,14 @@ git tag "${tag}"
 msbuild
 msbuild /p:DefineConstants="CHROMPER_11"
 
+pushd ChroMapper-PropEdit/bin/Dev
+zip "ChroMapper-12-PropEdit ${tag}.zip" Plugins/ChroMapper-PropEdit.dll
+popd
+
+pushd ChroMapper-PropEdit/bin/Stable
+zip "ChroMapper-11-PropEdit ${tag}.zip" Plugins/ChroMapper-PropEdit.dll
+popd
+
 echo "Check..."
 read || exit
 
